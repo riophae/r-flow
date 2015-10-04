88eddf12c88135bf5bd44c95e37d42ca32bf2e8e
Riophae Lee <riophaelee@gmail.com>
2015-10-05 00:30:43 +0800

    add an extra blank line

--- utils/replaceString.js
+++ utils/replaceString.js
@@ -2,6 +2,7 @@ var toArray = require('./toArray');
 
 module.exports = function replaceString(rawString) {
   var args = toArray(arguments, 1);
+
   return args.reduce(function(string, arg) {
     return string.replace(/%[ds]/, arg);
   }, rawString);
